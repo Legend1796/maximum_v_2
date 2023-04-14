d0332f35e552466e626683e5af77ec94d4bdeb5f
Igor Manukhov <m_igor97@mail.ru>
2023-04-14 23:27:34 +0300

    carousel

--- src/components/individual/individual.jsx
+++ src/components/individual/individual.jsx
@@ -4,12 +4,44 @@ import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 
 const Individual = () => {
+  const options = {
+    loop: true,
+    center: true,
+    items: 1,
+    margin: 0,
+    dots: true,
+    nav: true,
+  };
+
   return (
     <section className="individual">
       <div className="individual__container">
         <h2 className="individual__title">Инвивидуальный подход к каждой поставке</h2>
         <h4 className="individual__subtitle">Примеры доставленного оборудования</h4>
-        <OwlCarousel>
+        <OwlCarousel className="owl-theme" {...options}>
+          <div class="item">
+            <div className="individual__item">
+              <div className="individual__column">
+                <div className="individual__image"></div>
+              </div>
+              <div className="individual__column">
+                <h2 className="individual__col-title">Невком</h2>
+                <h6 className="individual__col-subtitle">Что везли?</h6>
+                <p className="individual__col-text">Вальцы для прокатки листового металла</p>
+                <h6 className="individual__col-subtitle">Особенности заказа</h6>
+                <p className="individual__col-text">
+                  Установка была значительных размеров. Размер вальцов определяется толщиной сгибаемого листа стали.
+                  Требуемые вальцы гнут сталь 60мм. Если бы фальцы гнули 4-6 мм, они были бы не такие большие.
+                </p>
+                <h6 className="individual__col-subtitle">Результат</h6>
+                <p className="individual__col-text">
+                  Собрали предложения от поставщиков и выбрали оптимального по соотношению цена/качество. В процессе
+                  поставки добавили в спецификацию поддерживающие утсройства и успешно импортировали оборудование.
+                  Клиент остался доволен.
+                </p>
+              </div>
+            </div>
+          </div>
           <div class="item">
             <div className="individual__item">
               <div className="individual__column">
