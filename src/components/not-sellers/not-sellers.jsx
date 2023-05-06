c5d33402ef2efeec25f2b4657e3c5fcecf683794
Igor Manukhov <m_igor97@mail.ru>
2023-05-06 21:33:25 +0300

    not sellers new

--- src/components/not-sellers/not-sellers.jsx
+++ src/components/not-sellers/not-sellers.jsx
@@ -11,41 +11,39 @@ const NotSellers = () => {
       <h5 className="not-sellers__subtitle">
         Мы - ваша правая рука в импорте необходимого оборудования и оказываем качественный сервис
       </h5>
-      <div className="not-sellers-container">
-        <ul className="not-sellers__cards">
-          <li className="not-sellers__card">
-            <img src={china} alt="china" className="not-sellers__photo" />
-            <h4 className="not-sellers__card-title">Лучшее предложение из Китая</h4>
-            <p className="not-sellers__card-text">
-              Мы знаем как общаться и строить бизнес с Китаем, благодаря чему вы получите лучшие цены и условия от
-              поставщика
-            </p>
-          </li>
-          <li className="not-sellers__card">
-            <img src={analyze} alt="analyze" className="not-sellers__photo" />
-            <h4 className="not-sellers__card-title">Надежные поставщики</h4>
-            <p className="not-sellers__card-text">
-              Мы проведем первоначальный или глубокий анализ надежности выбранного поставщика
-            </p>
-          </li>
-          <li className="not-sellers__card">
-            <img src={zip} alt="zip" className="not-sellers__photo" />
-            <h4 className="not-sellers__card-title">Образцы</h4>
-            <p className="not-sellers__card-text">
-              При необходимости заказываем образцы товаров или помогаем в предоставлении образцов зарубежному
-              производителю
-            </p>
-          </li>
-          <li className="not-sellers__card">
-            <img src={pig} alt="pig" className="not-sellers__photo" />
-            <h4 className="not-sellers__card-title">Экономия</h4>
-            <p className="not-sellers__card-text">
-              Учтем все нюансы таможенного оформления и поможем сэкономить за счет законной оптимизации таможенных
-              платежей
-            </p>
-          </li>
-        </ul>
-      </div>
+      <ul className="not-sellers__cards">
+        <li className="not-sellers__card">
+          <img src={china} alt="china" className="not-sellers__photo" />
+          <h4 className="not-sellers__card-title">Лучшее предложение из Китая</h4>
+          <p className="not-sellers__card-text">
+            Мы знаем как общаться и строить бизнес с Китаем, благодаря чему вы получите лучшие цены и условия от
+            поставщика
+          </p>
+        </li>
+        <li className="not-sellers__card">
+          <img src={analyze} alt="analyze" className="not-sellers__photo" />
+          <h4 className="not-sellers__card-title">Надежные поставщики</h4>
+          <p className="not-sellers__card-text">
+            Мы проведем первоначальный или глубокий анализ надежности выбранного поставщика
+          </p>
+        </li>
+        <li className="not-sellers__card">
+          <img src={zip} alt="zip" className="not-sellers__photo" />
+          <h4 className="not-sellers__card-title">Образцы</h4>
+          <p className="not-sellers__card-text">
+            При необходимости заказываем образцы товаров или помогаем в предоставлении образцов зарубежному
+            производителю
+          </p>
+        </li>
+        <li className="not-sellers__card">
+          <img src={pig} alt="pig" className="not-sellers__photo" />
+          <h4 className="not-sellers__card-title">Экономия</h4>
+          <p className="not-sellers__card-text">
+            Учтем все нюансы таможенного оформления и поможем сэкономить за счет законной оптимизации таможенных
+            платежей
+          </p>
+        </li>
+      </ul>
     </section>
   );
 };
