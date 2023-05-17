9dba464ba675b351a45dc6d378560f53f2ad348c
Igor Manukhov <m_igor97@mail.ru>
2023-05-17 22:51:46 +0300

    MAP

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -5,11 +5,10 @@ const Map = () => {
     <div className="map">
       <iframe
         className="map__content"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11653.545554684148!2d30.421982569177427!3d59.89182948083917!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46962fcfb8a352d9%3A0xb82e71104897c413!2z0YPQuy4g0J7Qu9GM0LPQuCDQkdC10YDQs9Cz0L7Qu9GM0YYsIDM2INC60L7RgNC_0YPRgSAyLCAxLCDQodCw0L3QutGCLdCf0LXRgtC10YDQsdGD0YDQsywgMTkyMTQ4!5e0!3m2!1sru!2sru!4v1683745780809!5m2!1sru!2sru"
-        // style={{ border: 0, width: 1000 }}
-        allowFullScreen=""
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
+        src="https://yandex.ru/map-widget/v1/?um=constructor%3A24a00f668189c7dc5ef84b2c4ca4aa6ec4634118a784c9e430cf151e5cfb97e4&amp;source=constructor"
+        width="539"
+        height="374"
+        frameborder="0"
         title="Карта"
       />
     </div>
